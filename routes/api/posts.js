f548cec56cb5d065cd62b873be9b2d71edb0bf44
Kyrune <43679939+Kyrune@users.noreply.github.com>
2020-02-05 11:03:23 -0600

    Add unlike route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -145,10 +145,41 @@ router.put("/like/:id", auth, async (req, res) => {
     if (
       post.likes.filter(like => like.user.toString() === req.user.id).length > 0
     ) {
-      return res.json(400).json({ msg: "Post already liked" });
+      return res.status(400).json({ msg: "Post already liked" });
     }
 
     post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Like a post
+// @access  Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has already been liked by the user
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: "Post has not yet been liked" });
+    }
+
+    // Get remove index
+    const removeIndex = post.likes
+      .map(like => like.usertoString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
 
     await post.save();
 
