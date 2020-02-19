631e499968bb7421605933b287dcca26bcfdcc06
Kyrune <43679939+Kyrune@users.noreply.github.com>
2020-02-19 10:37:25 -0600

    Add comment

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,6 +2,7 @@ import uuid from "uuid";
 import { SET_ALERT, REMOVE_ALERT } from "./types";
 
 export const setAlert = (msg, alertType) => dispatch => {
+  // Generate id
   const id = uuid.v4();
   dispatch({
     type: SET_ALERT,
