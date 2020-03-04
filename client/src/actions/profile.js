573cecb20cf2a50fd0dc35aed697f75a4618b1fd
Kyrune <43679939+Kyrune@users.noreply.github.com>
2020-03-04 10:33:11 -0600

    Fix typo in config headers

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -29,7 +29,7 @@ export const createProfile = (
   try {
     const config = {
       headers: {
-        "Content-Type": "application.json"
+        "Content-Type": "application/json"
       }
     };
 
@@ -40,7 +40,7 @@ export const createProfile = (
       payload: res.data
     });
 
-    dispatch(setAlert(edit ? "Profile Updated" : "Profile Created"));
+    dispatch(setAlert(edit ? "Profile Updated" : "Profile Created", "success"));
 
     if (!edit) {
       history.push("/dashboard");
