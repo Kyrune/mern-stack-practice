62ba4bb694ae32993107c9abba21e4cd08c62fc2
Kyrune <jn-01@centurytel.net>
2020-04-16 10:42:42 -0500

    Add JSX

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import { getProfileById } from '../../actions/profile';
 
-const Profile = props => {
+const Profile = ({ getProfileById, profile: { profile, loading }, auth,  match }) => {
+    useEffect(() => {
+        getProfileById(match.params.id);
+    }, [getProfileById]);
+
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <Fragment>
+            {profile === null || loading ? <Spinner /> : <Fragment>
+                    <Link to="/profiles" className="btn btn-light">
+                        Back To Profiles
+                    </Link>
+                    {/* Only shows if it's the logged in user's profile */}
+                    {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (<Link to="/edit-profile" className="btn btn-dark">Edit Profile</Link>)}
+                </Fragment>}
+        </Fragment>
+    );
+};
 
 Profile.propTypes = {
+    getProfileById: PropTypes.func.isRequired,
+    profile: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
+};
 
-}
+const mapStateToProps = state => ({
+    profile: state.profile,
+    auth: state.auth
+});
 
-export default Profile
+export default connect(mapStateToProps, { getProfileById })(Profile);
