71a8a6e62c8dd9c394dcb74a6b207f5b9ef385b0
Kyrune <jn-01@centurytel.net>
2020-04-16 09:47:28 -0500

    Create Profiles component

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Profiles = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Profiles.propTypes = {
+
+}
+
+export default Profiles
