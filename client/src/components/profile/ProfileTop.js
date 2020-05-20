f409e06024f9d7a18984621c7276a5215ac89d82
Kyrune <jn-01@centurytel.net>
2020-05-20 13:28:14 -0500

    Remove duplicate location

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -19,7 +19,7 @@ const ProfileTop = ({
         {status} {company && <span> at {company}</span>}
       </p>
       <p>
-        {location} {company && <span>{location}</span>}{" "}
+        {location && <span>{location}</span>}
       </p>
       <div className="icons my-1">
         {website && (
