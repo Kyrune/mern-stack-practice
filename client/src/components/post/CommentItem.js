3eed0307a2eb8df286617fe6e8c8f0f082c9e6be
Kyrune <jn-01@centurytel.net>
2020-05-21 12:07:27 -0500

    Remove Delete text & change bg to dark

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -12,7 +12,7 @@ const CommentItem = ({
   deleteComment
 }) => {
   return (
-    <div className="post bg-white p-1 my-1">
+    <div className="post bg-dark p-1 my-1">
       <div>
         <Link to={`/profile/${user}`}>
           <img className="round-img" src={avatar} alt="" />
@@ -30,7 +30,7 @@ const CommentItem = ({
             type="button"
             className="btn btn-danger"
           >
-            <i className="fas fa-times"></i>Delete
+            <i className="fas fa-times"></i>
           </button>
         )}
       </div>
